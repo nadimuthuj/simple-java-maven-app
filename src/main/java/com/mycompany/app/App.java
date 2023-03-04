4727badfa2f83f3bb0fbe3a0e999eba37e2c2ceb
nadimuthuj <naaadimuthu@gmail.com>
2023-03-05 01:04:14 +1100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,9 @@ package com.mycompany.app;
 public class App {
 
     private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE1 = "Hello World!";
 
+    
     public App() {}
 
     public static void main(String[] args) {
